2e41f23f62d2999a6e80caef49dde8c80b168d84
pietbinon <pierrebinon@icloud.com>
2017-01-20 16:15:03 -0500

    Initial Commit

---
+++ BillSplitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BillSplitter
+//
+//  Created by Pierre Binon on 2017-01-20.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

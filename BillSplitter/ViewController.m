0ec771382830af83e8c71616fdec3344b4341ddc
pietbinon <pierrebinon@icloud.com>
2017-01-20 17:06:29 -0500

    commit jan 20

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -8,9 +8,19 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfGuestsSlider;
+@property (weak, nonatomic) IBOutlet UILabel *dividedAmountOfBillLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+
 @end
+
+
+
+
 
 @implementation ViewController
 
@@ -23,6 +33,29 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)sliderChanged:(UISlider *)sender {
+    int progress = (int) lroundf(sender.value);
+    self.sliderLabel.text = [NSString stringWithFormat: @"%d", progress];
+}
+
+
+- (void) calculateIndividualAmountAndUpdateLabel {
+    //Configure variables to do math
+    double billAmount = self.billAmountTextField.text.doubleValue;
+    double numberOfEaters = self.numberOfGuestsSlider.value;
+    
+    //Do the math
+    double totalAmount = billAmount / numberOfEaters;
+    
+    //Update the labels in the view
+    self.dividedAmountOfBillLabel.text = [NSString stringWithFormat: @"$%.2f", totalAmount];
+}
+
+- (IBAction)calculateSplitAmount:(UIButton *)sender {
+    [self calculateIndividualAmountAndUpdateLabel];
 }
 
 
